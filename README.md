f20738aaf8ba4452200b52208e940eb98acacd33
Orie Steele <orie@transmute.industries>
2022-10-08 09:02:19 -0500

    :rocket:

---
+++ README.md
@@ -0,0 +1,17 @@
+# JSON-LD to Cypher
+
+<img src="./transmute-banner.png" />
+
+#### [Questions? Contact Transmute](https://transmute.typeform.com/to/RshfIw?typeform-source=did-eqt)
+
+#### 🚧 Warning Experimental 🔥
+
+### Testing CLI
+
+```
+npm run jsonld-to-cypher convert ./docs/example.json
+```
+
+### Installing Neo4j Desktop
+
+See [https://neo4j.com](https://neo4j.com/).
---
+++ src/cli.js
@@ -0,0 +1,35 @@
+#!/usr/bin/env node
+
+// const fs = require('fs');
+// const path = require('path');
+const yargs = require('yargs');
+const {hideBin} = require('yargs/helpers');
+
+// const readJsonFromPath = (argv, argName) => {
+//   let value;
+//   if (argv[argName]) {
+//     try {
+//       const file = fs
+//           .readFileSync(path.resolve(process.cwd(), argv[argName]))
+//           .toString();
+//       value = JSON.parse(file);
+//     } catch (e) {
+//       console.error('Cannot read from file: ' + argv[argName]);
+//       process.exit(1);
+//     }
+//   }
+//   return value;
+// };
+
+yargs(hideBin(process.argv))
+    .scriptName('jsonld-to-cipher')
+    .command(
+        'convert',
+        'transform jsonld into cypher query',
+        () => {},
+        async (argv) => {
+          console.log('echo ', JSON.stringify(argv, null, 2));
+        },
+    )
+    .demandCommand(1)
+    .parse();
