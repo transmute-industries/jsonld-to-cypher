9013c308bf87eeeda5c535e5d218fff3bc132d14
Orie Steele <orie@transmute.industries>
2022-10-08 09:31:38 -0500

    :rocket:

--- src/test.js
+++ src/test.js
@@ -1,7 +1,9 @@
+const fs = require('fs');
 const magic = require('./index')
 
 ;(async () => {
   console.log('🌴 testing library...');
   const cypher = await magic.convert(require('../docs/simpler-types.json'));
-  console.log(cypher.query);
+  const markdown = '# Generated Cypher\n```cypher\n' + cypher.query + '```';
+  fs.writeFileSync('./docs/cypher-query.md', markdown);
 })();
