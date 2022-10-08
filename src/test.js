a294af5cf18070fbc2a2f6a133e7470ca8051b86
Orie Steele <orie@transmute.industries>
2022-10-08 13:18:26 -0500

    :rocket:

--- src/test.js
+++ src/test.js
@@ -2,20 +2,35 @@
 const fs = require('fs');
 const {Graph, documentLoader} = require('./index')
 
+// ;(async () => {
+//   console.log('🌴 testing library...');
+//   const intermediate = await Graph.documentToGraph(
+//       require('../docs/simpler-types.json'),
+//       {documentLoader},
+//   );
+//   fs.writeFileSync(
+//       './docs/intermediate.json',
+//       JSON.stringify(intermediate, null, 2),
+//   );
+//   const cypher = await Graph.graphToCypher(intermediate);
+//   const markdown =
+//     '# Clear Database\n```cypher\nMATCH (n) DETACH DELETE n\n```\n# Generated Cypher\n```cypher\n' +
+//     cypher.query +
+//     '```';
+//   fs.writeFileSync('./docs/cypher-query.md', markdown);
+// })()
+
 ;(async () => {
   console.log('🌴 testing library...');
   const intermediate = await Graph.documentToGraph(
-      require('../docs/simpler-types.json'),
+      require('../docs/advanced-types.json.json'),
       {documentLoader},
   );
-
   fs.writeFileSync(
       './docs/intermediate.json',
       JSON.stringify(intermediate, null, 2),
   );
-
   const cypher = await Graph.graphToCypher(intermediate);
-
   const markdown =
     '# Clear Database\n```cypher\nMATCH (n) DETACH DELETE n\n```\n# Generated Cypher\n```cypher\n' +
     cypher.query +
