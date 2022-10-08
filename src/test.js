dc76edcd2fc9bc42758b3b76c89eb291e4f09172
Orie Steele <orie@transmute.industries>
2022-10-08 09:21:56 -0500

    :rocket:

--- src/test.js
+++ src/test.js
@@ -3,5 +3,5 @@ const magic = require('./index')
 ;(async () => {
   console.log('🌴 testing library...');
   const cypher = await magic.convert(require('../docs/example.json'));
-  console.log(cypher);
+  console.log(cypher.query);
 })();
