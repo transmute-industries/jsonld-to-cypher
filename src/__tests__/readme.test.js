808b513581689e74bfda9cd074f025d7fd71a60c
Orie Steele <orie@transmute.industries>
2022-10-30 16:43:40 -0500

    Cleanup

--- src/__tests__/readme.test.js
+++ src/__tests__/readme.test.js
@@ -1,6 +1,8 @@
 const {Cypher, GraphViz} = require('..');
 const fixtures = require('../__fixtures__');
 
+jest.setTimeout(10 * 1000);
+
 it('tests', async () => {
   const c1 = await Cypher.fromDocument(fixtures.revocableCredential);
   // console.log(c1);
