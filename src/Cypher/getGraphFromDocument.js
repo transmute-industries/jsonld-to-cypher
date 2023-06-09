be5a7d082d1818932d0492a20e6efb9a2520b953
Orie Steele <orie@transmute.industries>
2023-06-08 20:21:13 -0500

    Fixes to graph

--- src/Cypher/getGraphFromDocument.js
+++ src/Cypher/getGraphFromDocument.js
@@ -1,8 +1,7 @@
 /* eslint-disable max-len */
 const jsonld = require('jsonld');
 const _ = require('lodash');
-const tripleRegex =
-  /^(?<subject>(<([^<>]+)>|^_:c14n\d+)) (?<predicate>(<([^<>]+)>)) (?<object>(.+))/;
+const {NQuads} = require('rdf-canonize');
 
 const getLabelFromIri = (iri) => {
   return _.startCase(iri.split('/').pop().split('#').pop());
@@ -79,106 +78,6 @@ const setNodeLabelFromEdges = (graph) => {
     }
   });
 };
-const removeEscapedQuotes = (str) => {
-  if (str.startsWith('"') && str.endsWith('"')) {
-    return str.substring(1, str.length - 1);
-  }
-  return str;
-};
-const getObjectValue = (str) => {
-  const [primitive] = str.split('^^');
-  if (str.includes('http://www.w3.org/2001/XMLSchema#integer')) {
-    return parseInt(removeEscapedQuotes(primitive));
-  }
-  if (str.includes('http://www.w3.org/2001/XMLSchema#double')) {
-    return parseFloat(removeEscapedQuotes(primitive));
-  }
-  if (str.includes('http://www.w3.org/2001/XMLSchema#boolean')) {
-    return removeEscapedQuotes(primitive) === 'true';
-  }
-  if (str.includes('http://www.w3.org/2001/XMLSchema#dateTime')) {
-    return removeEscapedQuotes(primitive);
-  }
-  return `${str.replace(/'/g, '\\\'')}`;
-};
-
-const isRdfNode = (str) => {
-  return str.startsWith('<') && str.endsWith('>');
-};
-
-const removeAngleBrackets = (str) => {
-  if (typeof str === 'string') {
-    if (isRdfNode(str)) {
-      return str.substring(1, str.length - 1);
-    }
-  }
-  return str;
-};
-
-const finalQuadValue = (data) => {
-  if (typeof data === 'string') {
-    return removeEscapedQuotes(removeAngleBrackets(data).trim());
-  }
-  return data;
-};
-const parseNQuad = (nquad) => {
-  const match = nquad.match(tripleRegex);
-  let {subject, predicate, object} = match.groups;
-  let objectGraph = object;
-  let objectType = '';
-  let objectValue = '';
-  if (object.includes('_:c14n')) {
-    objectGraph = `_:c14n${object.split('_:c14n')[1]}`;
-    objectType = object.split(objectGraph)[0].trim();
-    if (objectType.includes('^^')) {
-      objectValue = getObjectValue(objectType) || '';
-      objectType = objectType.split('^^')[1].trim();
-    } else {
-      objectValue = getObjectValue(objectType) || '';
-      objectType = '';
-    }
-  }
-  if (object.includes('^^')) {
-    objectValue = getObjectValue(object) || '';
-    objectType = object.split('^^')[1].trim();
-    objectGraph = subject;
-  }
-  subject = finalQuadValue(subject);
-  predicate = finalQuadValue(predicate);
-  objectGraph = finalQuadValue(objectGraph);
-  objectType = finalQuadValue(objectType);
-  objectValue = finalQuadValue(objectValue);
-  if (objectType === '') {
-    objectType = undefined;
-  }
-  if (objectValue === '') {
-    objectValue = undefined;
-  }
-  return JSON.parse(
-      JSON.stringify({
-        subject: subject,
-        predicate: predicate,
-        object: objectGraph,
-        objectType: objectType,
-        objectValue: objectValue,
-      }),
-  );
-};
-
-const getNQuads = async (doc, documentLoader) => {
-  const canonized = await jsonld.canonize(doc, {
-    algorithm: 'URDNA2015',
-    format: 'application/n-quads',
-    documentLoader,
-    safe: false, // lossy behavior but doesn't fail
-  });
-  return canonized
-      .split('\n')
-      .filter((r) => r !== '')
-      .map((r) => {
-        return r.substring(0, r.length - 2);
-      });
-};
 
 const addGraphNode = ({graph, id}) => {
   graph.nodes[id] = {
@@ -206,101 +105,24 @@ const addGraphEdge = ({graph, source, target, label, definition}) => {
   );
 };
 
-const updateObjectType = ({
-  graph,
-  subject,
-  predicate,
-  object,
-  objectType,
-  objectValue,
-}) => {
-  if (isIri(objectType)) {
-    addGraphNode({graph, id: objectType});
-    addGraphEdge({
-      graph,
-      label: getLabelFromIri(objectType),
-      source: object,
-      definition: predicate,
-      target: objectType,
-    });
-  }
-};
-
-const updateObjectValue = ({
-  graph,
-  subject,
-  predicate,
-  object,
-  objectType,
-  objectValue,
-}) => {
-  addGraphNodeProperty(graph, object, predicate, objectValue);
-  if (isIri(objectValue)) {
-    addGraphNode({graph, id: objectValue});
-    if (predicate === 'http://www.w3.org/1999/02/22-rdf-syntax-ns#type') {
-      addGraphEdge({
-        graph,
-        label: getLabelFromIri(objectValue),
-        source: subject,
-        definition: objectValue,
-        target: object,
-      });
-      addGraphEdge({
-        graph,
-        label: getLabelFromIri(predicate),
-        source: subject,
-        definition: predicate,
-        target: objectValue,
-      });
-    } else {
-      addGraphEdge({
-        graph,
-        label: getLabelFromIri(predicate),
-        source: object,
-        definition: predicate,
-        target: objectValue,
-      });
-    }
-  }
-  // console.log('🔥', {subject, predicate, object, objectType, objectValue});
-};
-
 const updateGraph = (graph, nquad) => {
-  const statement = parseNQuad(nquad);
-  // console.log(statement);
-  const {subject, predicate, object, objectType, objectValue} = statement;
-  addGraphNode({graph, id: subject});
-  // uncomment to add predicates as nodes
-  // addGraphNode({graph, id: predicate});
-  addGraphNode({graph, id: object});
-  if (!objectType && !objectValue) {
+  addGraphNode({graph, id: nquad.subject.value});
+  if (!nquad.object.datatype) {
+    addGraphNode({graph, id: nquad.object.value});
     addGraphEdge({
       graph,
-      label: getLabelFromIri(predicate),
-      source: subject,
-      definition: predicate,
-      target: object,
+      label: getLabelFromIri(nquad.predicate.value),
+      source: nquad.subject.value,
+      definition: nquad.predicate.value,
+      target: nquad.object.value,
     });
-  }
-  if (objectType) {
-    updateObjectType({
-      graph,
-      subject,
-      predicate,
-      object,
-      objectType,
-      objectValue,
-    });
-  }
-  if (objectValue) {
-    updateObjectValue({
-      graph,
-      subject,
-      predicate,
-      object,
-      objectType,
-      objectValue,
-    });
+  } else {
+    addGraphNodeProperty(
+        graph,
+        nquad.subject.value,
+        nquad.predicate.value,
+        nquad.object.value,
+    );
   }
 };
 
@@ -339,26 +161,102 @@ const requireAbsoluteNodes = (graph) => {
   });
 };
 
+const fixPresentationGraph = (graph) => {
+  const search = (e) =>
+    e.definition === 'https://www.w3.org/2018/credentials#verifiableCredential';
+  const index = graph.links.findIndex(search);
+  const edge = graph.links[index];
+  graph.links.splice(index, 1);
+  delete graph.nodes[edge.target];
+  return graph;
+};
+
 const getGraphFromDocument = async (doc, {documentLoader, id}) => {
   const nodes = {};
   const links = [];
+  if (doc.id) {
+    id = doc.id;
+  }
   const graph = {id, nodes, links};
-  const nquads = await getNQuads(doc, documentLoader);
-  nquads.forEach((nquad) => {
+  const proofs = {};
+  const credentialIds = [];
+  const document = structuredClone(doc);
+  if (document.proof) {
+    proofs[document.id] = document.proof;
+    delete document.proof;
+  }
+  // TODO: refactor to remove @container sadness
+  if (document.verifiableCredential) {
+    document.verifiableCredential.forEach((credential) => {
+      proofs[credential.id] = credential.proof;
+      delete credential.proof;
+      credentialIds.push(credential.id);
+    });
+    // delete document.verifiableCredential;
+  }
+  const canonized = await jsonld.canonize(document, {
+    algorithm: 'URDNA2015',
+    format: 'application/n-quads',
+    documentLoader,
+    safe: false, // lossy behavior but doesn't fail
+  });
+  const parsedNquads = NQuads.parse(canonized);
+  parsedNquads.forEach((nquad) => {
     updateGraph(graph, nquad);
   });
-  // console.log(nquads);
+  const keys = Object.keys(proofs);
+  for (let i = 0; i < keys.length; i++) {
+    const id = keys[i];
+    const proof = proofs[id];
+    const proofId = proof.id || id + ':proof:' + i;
+    addGraphNode({graph, id: proofId});
+    graph.nodes[proofId] = {
+      ...graph.nodes[proofId],
+      ...proof,
+      labels: [proof.type],
+    };
+    addGraphEdge({
+      graph,
+      label: 'proof',
+      source: id,
+      definition: 'https://w3id.org/security#proof',
+      target: proofId,
+    });
+  }
+  if (credentialIds.length) {
+    credentialIds.forEach((credentialId) => {
+      addGraphEdge({
+        graph,
+        label: 'Verifiable Credential',
+        source: document.id,
+        definition: 'https://www.w3.org/2018/credentials#verifiableCredential',
+        target: credentialId,
+      });
+    });
+  }
   setNodeLabelFromEdges(graph);
+  fixPresentationGraph(graph);
   graph.nodes = Object.values(graph.nodes);
   graph.nodes = graph.nodes.map((n) => {
     const defaultLabel = getNodeLabel(n);
-    const finalLabels =
+    let finalLabels =
       n.labels.length >= 1 && defaultLabel === 'Blank Node' ?
         n.labels :
         [defaultLabel, ...n.labels];
 
+    const typeEdge = graph.links.find(
+        (e) =>
+          e.source === n.id &&
+        e.definition === 'http://www.w3.org/1999/02/22-rdf-syntax-ns#type',
+    );
+
+    if (typeEdge && typeEdge.target.includes('VerifiablePresentation')) {
+      finalLabels = ['VerifiablePresentation', ...finalLabels];
+    }
+
     return {...n, labels: finalLabels};
   });
+
   removeEmptyBlankNodes(graph);
   requireAbsoluteNodes(graph);
   return graph;
